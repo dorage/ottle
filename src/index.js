ec2ad07b4e37f19cdad162e4b46345ae50ba8f41
HOME-DESKTOP <baloonflower554@gmail.com>
2022-08-04 21:20:53 +0900

    sentry 추가

--- src/index.js
+++ src/index.js
@@ -10,6 +10,19 @@ import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { Modal } from './components/Modal';
 import { Alert } from './components/Alert';
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
+
+Sentry.init({
+    dsn:
+        'https://6ccc0d4e27364b14b72795f9e6dc039d@o416022.ingest.sentry.io/6624563',
+    integrations: [new BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+});
 
 const container = document.getElementById('app-root');
 const root = createRoot(container);
