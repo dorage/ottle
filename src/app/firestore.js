44389bde98210e4833f940c389140d18a27f8b30
HOME-DESKTOP <baloonflower554@gmail.com>
2022-08-07 01:59:14 +0900

    for testing

--- src/app/firestore.js
+++ src/app/firestore.js
@@ -278,6 +278,9 @@ export const getOttlesByUID = paginationHoC(
             startAfter(getRef())
         );
 
+        console.log(querySnapshot.empty);
+        console.log(querySnapshot.docs);
+
         if (querySnapshot.empty) return [];
         setRef(_.getLastIndex(querySnapshot.docs));
 
